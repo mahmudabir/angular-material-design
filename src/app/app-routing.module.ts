801d3c93949bfe454bab6620dafdc6d09a9c9334
Abir Mahmud <amabirmahmud@gmail.com>
2021-06-16 16:44:22 +0600

    @angular/material installed
    ng add @angular/material

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: ' ', redirectTo: 'homepage' },
+  { path: 'homepage', component: AppComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
